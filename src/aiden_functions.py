6e30f1df9f2dc9a743a8ae1c90f3b7ea8f534194
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2026-05-06 16:47:41 +0000

    working on minesweeper

--- src/aiden_functions.py
+++ src/aiden_functions.py
@@ -3,33 +3,46 @@
 import random
 
 def board_maker(board_width, board_height, minbombs = 10, maxbombs = 15):
-    row = []
-    for x in range(0, board_width):
-        row.append(0)
 
-    board = [
-
-    ]
-    for y in range(0, board_height):
-        board.append(row)
+    board = []
     
-    for x in range(0, len(board)):
-        for y in range(0, len(board[x])):
+    for y in range(0, board_height):
+        row = []
+        for x in range(0, board_width):
             number = random.randint(1, 6)
             if number == 6:
-                print(x)
-                board[x][y] = 1
+                row.append(1)
             else:
-                pass
-#    
-#    for each tile on board
-#        if the tile is a bomb
-#            move on
-#        otherwise
-#            check all nearby tiles for bombs
-#                if the 
+                row.append(0)
+        board.append(row)
+    
+    detection = board
+    for line in detection:
+        for tile in range(0, len(line)):
+            line[tile] = 0
 
-    for line in board:
+    for x in range(0, len(detection)):
+        d_line = detection(x)
+        for y in range(0, len(line)):
+            if board[x][y] == 1:
+                pass
+            else:
+                bomb_total = 0
+                behind = False
+                forward = False
+                up = False
+                down = False
+                if y-1 < 0:
+                    behind = True
+                if y+1 < 0:
+                    forward = True
+                if y-1 < 0:
+                    up = True
+                if y-1 < 0:
+                    down = True
+
+
+    for line in detection:
         print(line)
 
 board_maker(10, 10)
